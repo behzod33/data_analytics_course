9918965917b21bc662c19c181a4d0a0d26fa8188
Behozod Jumaev <behzodjumaev33@gmail.com>
2024-11-10 10:36:43 +0500

    Добавил зависимости для работы с базой в plotly

---
+++ connector.py
@@ -0,0 +1,17 @@
+import psycopg2
+import sqlalchemy as db
+
+def read_credentials():
+    with open('credentials.txt') as f:
+        connection_string = f.read()
+    return connection_string
+
+def connect_to(db_name):
+    if db_name == 'postgres':
+        with open('credentials.txt') as f:
+            connection_string = f.read()
+        engine = db.create_engine(connection_string)
+        conn = engine.connect()
+    
+    return conn
+
